2c1d255aeb7563a98b8e4dd9f8470d09aa5b91f0
Zaid Al-ruwaishan <zaidruwaishan@gmail.com>
2025-04-21 17:17:14 -0400

    change prettyPrint to print board from white pov

--- utilities.cpp
+++ utilities.cpp
@@ -1,10 +1,13 @@
 #include "utilities.h"
 
 void prettyPrint(Bitboard bb) {
-    for (int i = 0; i < 64; i++) {
-        if (i % 8 == 0) std::cout << '\n';
-        if ((bb >> i) & 0b1) std::cout << "1 ";
-        else std::cout << "0 ";    
+    for (int rank = 7; rank >= 0; rank--) {
+        for (int file = 0; file < 8; file++) {
+            Bitboard squareBB = (Bitboard)1 << (rank * 8 + file);
+            if (bb & squareBB) std::cout << "1 ";
+            else std::cout << "0 ";
+        }
+        std::cout << '\n';
     }
     std::cout << "\n\n";
 }
