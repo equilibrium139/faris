92775439edf96de24d0dcf909b426ed31bdb6eb6
Zaid Al-ruwaishan <30693070+equilibrium139@users.noreply.github.com>
2025-06-12 16:13:46 +0000

    generate bishop magics and output header file for engine to consume

--- tools/magic/magicgen.cpp
+++ tools/magic/magicgen.cpp
@@ -1,26 +1,33 @@
-#include <algorithm>
 #include <bit>
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 #include "board.h"
 #include "utilities.h"
 
 static constexpr int rookMoves[4] = { 8, -8, 1, -1 };
-static constexpr int rookShift = 64 - 12;
+static constexpr int rookShift = 64 - 12; // 12 is upper bound for popcount(rookMasks[i])
 static constexpr int bishopMoves[4] = { 7, 9, -7, -9 };
+static constexpr int bishopShift = 64 - 9; // 9 is upper bound for popcount(bishopMasks[i])
 
 static std::uint64_t rookMagics[64];
 static Bitboard rookMasks[64];
 static Bitboard rookAttacks[64][4096];
 
+static std::uint64_t bishopMagics[64];
+static Bitboard bishopMasks[64];
+static Bitboard bishopAttacks[64][512];
 
-static bool OnEdge(Square square) {
+static bool BishopReachedEdge(Square square, int move) {
     int file = square % 8;
     int rank = square / 8;
-    return file == 0 || file == 7 || rank == 0 || rank == 7; 
+    return move == 7 && (file == 0 || rank == 7) ||
+           move == 9 && (file == 7 || rank == 7) ||
+           move == -7 && (file == 7 || rank == 0) ||
+           move == -9 && (file == 0 || rank == 0);
 }
 
 static bool RookReachedEdge(Square square, int move) {
@@ -61,18 +68,18 @@ static Bitboard GenRookMask(Square square) {
             from = s;
         }
     }
-
     return mask;
 }
 
 static Bitboard GenBishopMask(Square square) {
     Bitboard mask = 0;
     for (int move : bishopMoves) {
-        for (Square s = square + move; !OnEdge(s) && ValidBishopMove(square, s); s += move) {
+        Square from = square;
+        for (Square s = square + move; !BishopReachedEdge(s, move) && ValidBishopMove(from, s); s += move) {
             mask |= ToBitboard(s);
+            from = s;
         }
     }
-
     return mask;
 }
 
@@ -106,22 +113,43 @@ static Bitboard GenRookAttackBitboard(Square square, Bitboard occupancy) {
     return attackBitboard;
 }
 
-static void GenRookAttackBitboardArray() {
+static Bitboard GenBishopAttackBitboard(Square square, Bitboard occupancy) {
+    Bitboard attackBitboard = 0;
+    for (int move : bishopMoves) {
+        if (!ValidBishopMove(square, square + move)) continue;
+        for (Square s = square + move; ; s += move) {
+            Bitboard sBB = ToBitboard(s);
+            attackBitboard |= sBB;
+            if ((occupancy & sBB) || BishopReachedEdge(s, move)) { 
+                break;
+            }
+        }
+    }
+    return attackBitboard;
+}
+
+static void GenAttackBitboardArray(bool rook) {
+    auto GenMaskFunc = rook ? GenRookMask : GenBishopMask;
+    Bitboard* masks = rook ? rookMasks : bishopMasks;
+    const unsigned int countPermutations = rook ? 4096 : 512;
+    auto GenAttackBitboardFunc = rook ? GenRookAttackBitboard : GenBishopAttackBitboard;
+    auto magics = rook ? rookMagics : bishopMagics;
+    const int shift = rook ? rookShift : bishopShift;
+
     for (Square square = 0; square < 64; square++) {
-        Bitboard mask = GenRookMask(square);
-        rookMasks[square] = mask;
+        auto squareAttacks = rook ? rookAttacks[square] : bishopAttacks[square];
+        Bitboard mask = GenMaskFunc(square);
+        masks[square] = mask;
         std::vector<int> oneIndices;
         for (int i = 0; i < 64; i++) {
             if ((1ULL << i) & mask) {
                 oneIndices.push_back(i);
             }
         }
-        // 4096 is an upper bound
-        constexpr std::uint32_t countPermutations = 4096;
 
-        bool idxUsed[countPermutations];
-        Bitboard occupancyPermutations[countPermutations];
-        Bitboard attackBitboards[countPermutations];
+        std::vector<bool> idxUsed(countPermutations, false);
+        std::vector<Bitboard> occupancyPermutations(countPermutations);
+        std::vector<Bitboard> attackBitboards(countPermutations);
         for (std::uint32_t i = 0; i < countPermutations; i++) {
             occupancyPermutations[i] = 0;
             for (std::uint32_t j = 0; j < oneIndices.size(); j++) {
@@ -129,7 +157,7 @@ static void GenRookAttackBitboardArray() {
                     occupancyPermutations[i] |= (1ULL << oneIndices[j]);
                 }
             }
-            attackBitboards[i] = GenRookAttackBitboard(square, occupancyPermutations[i]);
+            attackBitboards[i] = GenAttackBitboardFunc(square, occupancyPermutations[i]);
         }
         std::uint64_t magic;
         while (true) {
@@ -137,23 +165,22 @@ static void GenRookAttackBitboardArray() {
             // see chessprogramming link above for this weirdness
             if (std::popcount((mask * magic) & 0xFF00000000000000ULL) < 6) { continue; }
             bool goodMagic = true;
-            std::fill(idxUsed, idxUsed + countPermutations, false);
+            idxUsed.assign(idxUsed.size(), false);
             for (std::uint32_t i = 0; i < countPermutations; i++) {
                 Bitboard occupancyPermutation = occupancyPermutations[i];
-                int permutationIdx = (occupancyPermutation * magic) >> rookShift;
+                int permutationIdx = (occupancyPermutation * magic) >> shift;
                 // bad collision = bad magic number
-                if (idxUsed[permutationIdx] && rookAttacks[square][permutationIdx] != attackBitboards[i]) {
+                if (idxUsed[permutationIdx] && squareAttacks[permutationIdx] != attackBitboards[i]) {
                     goodMagic = false;
                     break;
                 }
                 else {
                     idxUsed[permutationIdx] = true;
-                    rookAttacks[square][permutationIdx] = attackBitboards[i];
+                    squareAttacks[permutationIdx] = attackBitboards[i];
                 }
             }
             if (goodMagic) {
-                std::cout << magic << std::endl;
-                rookMagics[square] = magic;
+                magics[square] = magic;
                 break;
             }
         }
@@ -162,11 +189,57 @@ static void GenRookAttackBitboardArray() {
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        // std::cerr << "Usage: magicgen <output_file>" << std::endl;            
-        // return 1;
+        std::cerr << "Usage: magicgen <output_file>" << std::endl;            
+        return 1;
     }
 
-    // std::string outputFile = argv[1];
-    // std::cout << outputFile << std::endl;
-    GenRookAttackBitboardArray();
+    std::string outputFilePath = argv[1];
+    std::ofstream outputFile{outputFilePath};
+
+    GenAttackBitboardArray(true);
+    GenAttackBitboardArray(false);
+
+    outputFile << "#pragma once\n"
+                  "#include <cstdint>\n"
+                  "static constexpr std::uint64_t rookMask[64] = {";
+
+    for (int i = 0; i < 64; i++) {
+        outputFile << rookMasks[i] << "ULL,";
+    }
+
+    outputFile << "};\nstatic constexpr std::uint64_t rookMagic[64] = {";
+    
+    for (int i = 0; i < 64; i++) {
+        outputFile << rookMagics[i] << "ULL,";
+    }
+
+    outputFile << "};\nstatic constexpr std::uint64_t rookAttacks[64][4096] = {";
+
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 4096; j++) {
+            outputFile << rookAttacks[i][j] << "ULL,";
+        }
+    }
+
+    outputFile << "};\nstatic constexpr std::uint64_t bishopMask[64] = {";
+
+    for (int i = 0; i < 64; i++) {
+        outputFile << bishopMasks[i] << "ULL,";
+    }
+
+    outputFile << "};\nstatic constexpr std::uint64_t bishopMagic[64] = {";
+    
+    for (int i = 0; i < 64; i++) {
+        outputFile << bishopMagics[i] << "ULL,";
+    }
+
+    outputFile << "};\nstatic constexpr std::uint64_t bishopAttacks[64][512] = {";
+
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 512; j++) {
+            outputFile << bishopAttacks[i][j] << "ULL,";
+        }
+    }
+
+    outputFile << "};\n";
 }
